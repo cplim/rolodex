79e0ae1510e70d6f552b630b3de5dbee7010a900
C.P. Lim <cheenpin.lim@gmail.com>
2025-12-09 22:00:56 +1100

    Added search for characters

--- src/Search.tsx
+++ src/Search.tsx
@@ -1,10 +1,32 @@
+import {ApolloClient, gql, HttpLink, InMemoryCache} from "@apollo/client";
 import {useSearchParams} from "react-router";
+import {useQuery} from "@apollo/client/react";
+
+const searchQuery = gql`
+    query GetCharacters($page: Int!, $search: String!) {
+        characters(page: $page, filter: {name: $search}) {
+            results {
+                id,
+                name
+            }
+        }
+    }`;
 
 const Search = () => {
     const [searchParams] = useSearchParams();
+    const variables = {search: searchParams.get('q'), page: 1};
+    const {loading, error, data} = useQuery(searchQuery, {
+        variables: variables
+    });
+
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error :(</p>;
     return(
       <div>
           <h1>You searched for: {searchParams.get('q')}</h1>
+          <ul>
+              {data.characters.results.map(character => <li key={character.id}>{character.name}</li>)}
+          </ul>
       </div>
     );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -4,14 +4,23 @@ import './index.css'
 import Home from './Home.tsx'
 import {BrowserRouter, Route, Routes} from "react-router";
 import Search from "./Search.tsx";
+import {ApolloProvider} from "@apollo/client/react";
+import {ApolloClient, HttpLink, InMemoryCache} from "@apollo/client";
+
+const client = new ApolloClient({
+    link: new HttpLink({uri: "https://rickandmortyapi.com/graphql"}),
+    cache: new InMemoryCache()
+});
 
 createRoot(document.getElementById('root')!).render(
-    <BrowserRouter>
-        <StrictMode>
-            <Routes>
-                <Route index element={<Home />} />
-                <Route path='search' element={<Search />} />
-            </Routes>
-        </StrictMode>
-    </BrowserRouter>
+    <ApolloProvider client={client}>
+        <BrowserRouter>
+            <StrictMode>
+                <Routes>
+                    <Route index element={<Home />} />
+                    <Route path='search' element={<Search />} />
+                </Routes>
+            </StrictMode>
+        </BrowserRouter>
+    </ApolloProvider>
 )
